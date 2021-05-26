29f6d8f005880ef1cf968058f1b7cad3be5f9f1e
Greg Cooper <gcooper2@nd.edu>
2021-05-25 21:27:23 -0400

    yes

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,7 @@ A python package for analysizing and visualizing xyz files
 """
 
 # Add imports here
-from .functions import *
+from .functions import calculate_distance
 
 # Handle versioneer
 from ._version import get_versions
